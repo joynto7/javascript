499d3d0b66677db86dfb31da386e412af6317f89
joynto7 <joyntoghosh7@gmail.com>
2024-09-03 00:36:56 +0600

    ternary

---
+++ ternary.js
@@ -0,0 +1,13 @@
+// TERNARY --> three parts
+
+const age = 18;
+if(age >=18){
+    console.log('you can vote.')
+}
+else{
+    console.log('you can not give vote')
+}
+
+// ternary
+
+const age =18 ? console.log('you can vote') : console.log('you can not give vote')
