fdf1ac234e64714641b93f3c7660f0e92bfac922
joynto7 <joyntoghosh7@gmail.com>
2024-09-02 23:21:52 +0600

    new if condition

--- multiple.js
+++ multiple.js
@@ -1,8 +1,20 @@
 // if....else condition
+
 const salary = 35000;
 const height = 6 ;
 
 if(salary>20000 && height>5){
+    console.log('He can marry now')
+}
+
+else(
+    console.log('Do not marry any girl')
+)
+
+const age = 30;
+const weight = 6;
+
+if(age>20000 || weight>5){
     console.log('He can marry now')
 }
 
