9d6fd8d18630b9a62bba0306bf93bfa0dfee9c94
joynto7 <joyntoghosh7@gmail.com>
2024-09-03 00:01:59 +0600

    Multiple conditon

---
+++ nested.js
@@ -0,0 +1,13 @@
+const money = 10;
+if(money>350){
+    console.log('bro you are rich')
+}
+else{
+    if(money > 100)
+        console.log('bro u are not rich');
+    else{
+        if(money > 0){
+            console.log('bro we should drink tea');
+        }
+    }
+}
