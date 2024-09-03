e47f305387f95a365eb6188c1515012e196af5cf
joynto7 <joyntoghosh7@gmail.com>
2024-09-03 17:44:54 +0600

    use of push and pop

---
+++ push-pop.js
@@ -0,0 +1,23 @@
+// numbers push
+
+const numbers = [25,37,36,49];
+console.log(numbers);
+numbers.push(83,64,39);
+console.log(numbers);
+
+// strings push
+
+const friends = ['oishake','tonmoy','lamia','maria'];
+friends.push('sakib');
+console.log(friends);
+
+// strings pop
+
+const fruits = ['mango','jackfruit','banana','lichi'];
+console.log(fruits);
+fruits.pop('sakib');
+console.log(fruits);
+
+
+
+
