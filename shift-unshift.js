c0573ee37d367dc41ad174927c7b0191f76b2f7d
joynto7 <joyntoghosh7@gmail.com>
2024-09-03 17:52:09 +0600

    use of shift and unshift

---
+++ shift-unshift.js
@@ -0,0 +1,8 @@
+// strings shift
+
+const flowers = ['hisbicus','rose','lily','cherry bollosm'];
+console.log(flowers);
+flowers.shift();
+console.log(flowers);
+flowers.unshift('black-rose');
+console.log(flowers)
