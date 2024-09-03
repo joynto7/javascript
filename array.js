e86f0457030586cc89aa2f93a0825c30afd1d3db
joynto7 <joyntoghosh7@gmail.com>
2024-09-03 17:20:01 +0600

    array

---
+++ array.js
@@ -0,0 +1,17 @@
+const numbers = [4,5,6,7];
+const friends = ['ovishake', 'Mondira', 'Tonmoy'];
+//  use of length
+console.log(friends.length);
+// use of index 
+
+console.log(friends[1]);
+const thrid = friends[1];
+
+// set or update element value by index
+
+let abc = 45;
+abc = 99;
+
+numbers[3] = 999;
+console.log(numbers);
+
